9475f69f6df92974b0621dcc4da2500e7ea8b9c0
David H <iq.davidh@gmail.com>
2019-08-27 03:05:34 -0500

    ya con react-boostrap

--- c1/src/App.js
+++ c1/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Form,FormControl,Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      text:''
+    };
+  }
+
+  render () {
+    return (
+        <div >
+          <h2>Note to self2</h2>
+          <Form>
+            <FormControl />
+            <Button>Submit</Button>
+          </Form>
+        </div>
+    );
+  }
 }
+
+
 
 export default App;
